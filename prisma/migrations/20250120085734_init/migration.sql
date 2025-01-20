3e77a559ac4aa92c07e7824703aff01b1bc3347d
wimartinezsa <180329990+wimartinezsa@users.noreply.github.com>
2025-01-20 17:05:10 -0500

    primera migracion

--- prisma/migrations/20250120085734_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `procedimientos` ADD COLUMN `precio` DECIMAL(10, 2) NULL DEFAULT 0;
--- prisma/migrations/20250120220053_init/migration.sql
+++ prisma/migrations/20250120220053_init/migration.sql
@@ -19,6 +19,9 @@ CREATE TABLE `pacientes` (
 
     UNIQUE INDEX `pacientes_identificacion_key`(`identificacion`),
     UNIQUE INDEX `pacientes_email_key`(`email`),
+    INDEX `pacientes_epsId_fkey`(`epsId`),
+    INDEX `pacientes_municipioId_fkey`(`municipioId`),
+    INDEX `pacientes_paisId_fkey`(`paisId`),
     PRIMARY KEY (`id_paciente`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -52,6 +55,8 @@ CREATE TABLE `facturas` (
     `contratoId` INTEGER NOT NULL,
     `pacienteId` INTEGER NOT NULL,
 
+    INDEX `facturas_contratoId_fkey`(`contratoId`),
+    INDEX `facturas_pacienteId_fkey`(`pacienteId`),
     PRIMARY KEY (`id_factura`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -63,6 +68,7 @@ CREATE TABLE `resultados` (
     `examenId` INTEGER NOT NULL,
     `parametroId` INTEGER NOT NULL,
 
+    INDEX `resultados_parametroId_fkey`(`parametroId`),
     UNIQUE INDEX `resultados_examenId_parametroId_key`(`examenId`, `parametroId`),
     PRIMARY KEY (`id_resultado`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -80,6 +86,8 @@ CREATE TABLE `examenes` (
     `procedimientoId` INTEGER NOT NULL,
     `facturaId` INTEGER NOT NULL,
 
+    INDEX `examenes_facturaId_fkey`(`facturaId`),
+    INDEX `examenes_procedimientoId_fkey`(`procedimientoId`),
     PRIMARY KEY (`id_examen`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -99,6 +107,7 @@ CREATE TABLE `departamentos` (
     `nombre` VARCHAR(50) NOT NULL,
     `paisId` INTEGER NOT NULL,
 
+    INDEX `departamentos_paisId_fkey`(`paisId`),
     PRIMARY KEY (`id_departamento`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -109,6 +118,7 @@ CREATE TABLE `municipios` (
     `nombre` VARCHAR(50) NOT NULL,
     `departamentoId` INTEGER NOT NULL,
 
+    INDEX `municipios_departamentoId_fkey`(`departamentoId`),
     PRIMARY KEY (`id_municipio`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -116,13 +126,14 @@ CREATE TABLE `municipios` (
 CREATE TABLE `servicios` (
     `id_servicio` INTEGER NOT NULL AUTO_INCREMENT,
     `nombre` VARCHAR(50) NOT NULL,
-    `precio` DECIMAL(10, 2) NOT NULL,
     `estado` ENUM('Activo', 'Inactivo') NOT NULL DEFAULT 'Activo',
     `grupo_servicio` ENUM('01', '02', '03', '04', '05') NOT NULL,
     `modalidad_atencion` ENUM('01', '02', '03', '04', '06', '07', '08', '09') NOT NULL,
     `prestadorId` INTEGER NOT NULL,
     `tipo_servicioId` INTEGER NOT NULL,
 
+    INDEX `servicios_prestadorId_fkey`(`prestadorId`),
+    INDEX `servicios_tipo_servicioId_fkey`(`tipo_servicioId`),
     PRIMARY KEY (`id_servicio`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -152,6 +163,7 @@ CREATE TABLE `tipo_resultados` (
     `nombre` VARCHAR(50) NOT NULL,
     `parametroId` INTEGER NOT NULL,
 
+    INDEX `tipo_resultados_parametroId_fkey`(`parametroId`),
     PRIMARY KEY (`id_tipo_resultado`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -164,6 +176,7 @@ CREATE TABLE `parametros` (
     `valor_referencia` VARCHAR(300) NULL,
     `procedimientoId` INTEGER NOT NULL,
 
+    INDEX `parametros_procedimientoId_fkey`(`procedimientoId`),
     PRIMARY KEY (`id_parametro`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -176,7 +189,12 @@ CREATE TABLE `procedimientos` (
     `finalidadId` INTEGER NOT NULL,
     `cupsId` INTEGER NOT NULL,
     `areaId` INTEGER NULL,
+    `precio` DECIMAL(10, 2) NULL DEFAULT 0.00,
 
+    INDEX `procedimientos_areaId_fkey`(`areaId`),
+    INDEX `procedimientos_cupsId_fkey`(`cupsId`),
+    INDEX `procedimientos_finalidadId_fkey`(`finalidadId`),
+    INDEX `procedimientos_servicioId_fkey`(`servicioId`),
     PRIMARY KEY (`id_procedimiento`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -186,6 +204,7 @@ CREATE TABLE `areas` (
     `nombre` VARCHAR(30) NOT NULL,
     `prestadorId` INTEGER NULL,
 
+    INDEX `areas_prestadorId_fkey`(`prestadorId`),
     PRIMARY KEY (`id_area`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -216,6 +235,7 @@ CREATE TABLE `usuarios` (
     `areaId` INTEGER NOT NULL,
 
     UNIQUE INDEX `usuarios_identificacion_key`(`identificacion`),
+    INDEX `usuarios_areaId_fkey`(`areaId`),
     PRIMARY KEY (`id_usuario`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -231,6 +251,7 @@ CREATE TABLE `empresas` (
     `municipioId` INTEGER NOT NULL,
 
     UNIQUE INDEX `empresas_nit_key`(`nit`),
+    INDEX `empresas_municipioId_fkey`(`municipioId`),
     PRIMARY KEY (`id_empresa`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -243,6 +264,7 @@ CREATE TABLE `contratos` (
     `estado` ENUM('Activo', 'Inactivo') NOT NULL DEFAULT 'Activo',
     `empresaId` INTEGER NOT NULL,
 
+    INDEX `contratos_empresaId_fkey`(`empresaId`),
     PRIMARY KEY (`id_contrato`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -255,17 +277,19 @@ CREATE TABLE `acuerdos` (
     `procedimientoId` INTEGER NOT NULL,
     `contratoId` INTEGER NOT NULL,
 
+    INDEX `acuerdos_contratoId_fkey`(`contratoId`),
+    INDEX `acuerdos_procedimientoId_fkey`(`procedimientoId`),
     PRIMARY KEY (`id_acuerdo`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `pacientes` ADD CONSTRAINT `pacientes_paisId_fkey` FOREIGN KEY (`paisId`) REFERENCES `paises`(`id_pais`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `pacientes` ADD CONSTRAINT `pacientes_epsId_fkey` FOREIGN KEY (`epsId`) REFERENCES `eps`(`id_eps`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `pacientes` ADD CONSTRAINT `pacientes_municipioId_fkey` FOREIGN KEY (`municipioId`) REFERENCES `municipios`(`id_municipio`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `pacientes` ADD CONSTRAINT `pacientes_epsId_fkey` FOREIGN KEY (`epsId`) REFERENCES `eps`(`id_eps`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `pacientes` ADD CONSTRAINT `pacientes_paisId_fkey` FOREIGN KEY (`paisId`) REFERENCES `paises`(`id_pais`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `facturas` ADD CONSTRAINT `facturas_contratoId_fkey` FOREIGN KEY (`contratoId`) REFERENCES `contratos`(`id_contrato`) ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -280,10 +304,10 @@ ALTER TABLE `resultados` ADD CONSTRAINT `resultados_examenId_fkey` FOREIGN KEY (
 ALTER TABLE `resultados` ADD CONSTRAINT `resultados_parametroId_fkey` FOREIGN KEY (`parametroId`) REFERENCES `parametros`(`id_parametro`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `examenes` ADD CONSTRAINT `examenes_procedimientoId_fkey` FOREIGN KEY (`procedimientoId`) REFERENCES `procedimientos`(`id_procedimiento`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `examenes` ADD CONSTRAINT `examenes_facturaId_fkey` FOREIGN KEY (`facturaId`) REFERENCES `facturas`(`id_factura`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `examenes` ADD CONSTRAINT `examenes_facturaId_fkey` FOREIGN KEY (`facturaId`) REFERENCES `facturas`(`id_factura`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `examenes` ADD CONSTRAINT `examenes_procedimientoId_fkey` FOREIGN KEY (`procedimientoId`) REFERENCES `procedimientos`(`id_procedimiento`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `departamentos` ADD CONSTRAINT `departamentos_paisId_fkey` FOREIGN KEY (`paisId`) REFERENCES `paises`(`id_pais`) ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -304,16 +328,16 @@ ALTER TABLE `tipo_resultados` ADD CONSTRAINT `tipo_resultados_parametroId_fkey` 
 ALTER TABLE `parametros` ADD CONSTRAINT `parametros_procedimientoId_fkey` FOREIGN KEY (`procedimientoId`) REFERENCES `procedimientos`(`id_procedimiento`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `procedimientos` ADD CONSTRAINT `procedimientos_servicioId_fkey` FOREIGN KEY (`servicioId`) REFERENCES `servicios`(`id_servicio`) ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `procedimientos` ADD CONSTRAINT `procedimientos_finalidadId_fkey` FOREIGN KEY (`finalidadId`) REFERENCES `finalidad`(`id_finalidad`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `procedimientos` ADD CONSTRAINT `procedimientos_areaId_fkey` FOREIGN KEY (`areaId`) REFERENCES `areas`(`id_area`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `procedimientos` ADD CONSTRAINT `procedimientos_cupsId_fkey` FOREIGN KEY (`cupsId`) REFERENCES `cups`(`id_cups`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `procedimientos` ADD CONSTRAINT `procedimientos_areaId_fkey` FOREIGN KEY (`areaId`) REFERENCES `areas`(`id_area`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `procedimientos` ADD CONSTRAINT `procedimientos_finalidadId_fkey` FOREIGN KEY (`finalidadId`) REFERENCES `finalidad`(`id_finalidad`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `procedimientos` ADD CONSTRAINT `procedimientos_servicioId_fkey` FOREIGN KEY (`servicioId`) REFERENCES `servicios`(`id_servicio`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `areas` ADD CONSTRAINT `areas_prestadorId_fkey` FOREIGN KEY (`prestadorId`) REFERENCES `prestadores`(`id_prestador`) ON DELETE SET NULL ON UPDATE CASCADE;
@@ -328,7 +352,7 @@ ALTER TABLE `empresas` ADD CONSTRAINT `empresas_municipioId_fkey` FOREIGN KEY (`
 ALTER TABLE `contratos` ADD CONSTRAINT `contratos_empresaId_fkey` FOREIGN KEY (`empresaId`) REFERENCES `empresas`(`id_empresa`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `acuerdos` ADD CONSTRAINT `acuerdos_procedimientoId_fkey` FOREIGN KEY (`procedimientoId`) REFERENCES `procedimientos`(`id_procedimiento`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `acuerdos` ADD CONSTRAINT `acuerdos_contratoId_fkey` FOREIGN KEY (`contratoId`) REFERENCES `contratos`(`id_contrato`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `acuerdos` ADD CONSTRAINT `acuerdos_contratoId_fkey` FOREIGN KEY (`contratoId`) REFERENCES `contratos`(`id_contrato`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `acuerdos` ADD CONSTRAINT `acuerdos_procedimientoId_fkey` FOREIGN KEY (`procedimientoId`) REFERENCES `procedimientos`(`id_procedimiento`) ON DELETE RESTRICT ON UPDATE CASCADE;
