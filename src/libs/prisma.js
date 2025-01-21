61484cca0955db734736ff62eaab6d2a0954769b
wimartinezsa <180329990+wimartinezsa@users.noreply.github.com>
2025-01-21 17:41:44 -0500

    nuevo modelo 3

--- src/libs/prisma.js
+++ src/libs/prisma.js
@@ -1,6 +1,6 @@
 import {PrismaClient} from '@prisma/client'
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({log: ['query', 'info', 'warn', 'error']});
 
-//{log: ['query', 'info', 'warn', 'error']}
+//
 
 export default prisma;
