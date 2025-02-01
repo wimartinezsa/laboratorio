6d4c535e2e0213c109e0ec888a8cfdd3b86a8c44
wimartinezsa <180329990+wimartinezsa@users.noreply.github.com>
2025-02-01 10:11:27 -0500

    cambiar el select

--- public/js/procedimientos.js
+++ public/js/procedimientos.js
@@ -32,6 +32,18 @@ var Frm_tipo_resultado = new bootstrap.Modal(document.getElementById('Frm_tipo_r
 });
 
 
+$(function () {
+    //Initialize Select2 Elements
+    $('.select2').select2()
+
+    //Initialize Select2 Elements
+    $('.select2bs4').select2({
+      theme: 'bootstrap4'
+    })
+
+});
+
+
 
 
 function listarProcedimientosServicioId(id_servicio){
